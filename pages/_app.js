02a274e157ff3d06729a94b24974eb8ff7cfac9a
Jeet009 <jeetmukherjee009@gmail.com>
2022-01-19 21:17:44 +0530

    fisrt commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import "../styles/globals.css";
-import MainTemplate from "../template/mainTemplate";
+import MainTemplate from "../template/MainTemplate";
 
 function MyApp({ Component, pageProps }) {
   return (
