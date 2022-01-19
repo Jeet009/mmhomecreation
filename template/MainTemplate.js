6ddbf2a6014d62f4ce13e370fa815be3829a20d8
Jeet009 <jeetmukherjee009@gmail.com>
2022-01-19 21:16:46 +0530

    fisrt commit

--- template/MainTemplate.js
+++ template/MainTemplate.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Head from "next/head";
-import styles from "./main.module.css";
 import NavbarComponent from "../components/NavbarComponent";
 
 function MainTemplate({ children }) {
